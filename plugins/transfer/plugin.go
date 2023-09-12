7e6a0fcca828f0ffc5e0692104a74dbd9892e8f3
Derek McGowan <derek@mcg.dev>
2023-09-11 21:16:16 -0700

    Move to use github.com/containerd/log
    Signed-off-by: Derek McGowan <derek@mcg.dev>

--- plugins/transfer/plugin.go
+++ plugins/transfer/plugin.go
@@ -23,12 +23,12 @@ import (
 	"github.com/containerd/containerd/diff"
 	"github.com/containerd/containerd/errdefs"
 	"github.com/containerd/containerd/leases"
-	"github.com/containerd/containerd/log"
 	"github.com/containerd/containerd/metadata"
 	"github.com/containerd/containerd/pkg/transfer/local"
 	"github.com/containerd/containerd/pkg/unpack"
 	"github.com/containerd/containerd/platforms"
 	"github.com/containerd/containerd/plugin"
+	"github.com/containerd/log"
 
 	// Load packages with type registrations
 	_ "github.com/containerd/containerd/pkg/transfer/archive"
